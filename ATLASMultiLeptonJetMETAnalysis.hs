a3c31b0fd995b6ded857ed03a7f9f1041d167ca8
Ian-Woo Kim <ianwookim@gmail.com>
2013-02-27 16:46:31 -0500

    separate out Format.hs

--- ATLASMultiLeptonJetMETAnalysis.hs
+++ ATLASMultiLeptonJetMETAnalysis.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE RecordWildCards, GADTs #-}
 
 import           Codec.Compression.GZip
-import           Control.Monad
+{- import           Control.Monad
 import qualified Data.ByteString.Lazy.Char8 as LB
 import           Data.Maybe
 import qualified Data.Text.Lazy as T
 import           Data.Text.Lazy.Builder 
 import           Data.Text.Format 
-import           System.Environment
+import           System.Environment -}
 import           System.FilePath
 import           System.IO 
-import           Text.Hastache
-import           Text.Hastache.Context
+-- import           Text.Hastache
+-- import           Text.Hastache.Context 
 -- 
 import           HEP.Parser.LHCOAnalysis.Parse
 -- 
 import           HEP.Physics.Analysis.ATLAS.SUSY
+import           HEP.Physics.Analysis.ATLAS.SUSY.Format
 -- 
 import Debug.Trace
 
+{-
 log10 x = log x / log 10
 
 getExponent10 x = floor (log10 x)
@@ -34,91 +36,165 @@ sciformat (Just x) =
      else "$" ++ ((T.unpack . toLazyText . fixed 2 . getBody10) x) ++ "\\times 10^{" 
                       ++ (show e) ++ "}$" 
 sciformat (Nothing) = "0"
+-}
 
-filelist = 
-  [ ( 1000, 400, 3.67e-3
-    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ400.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+
+filelist1000 = 
+  [ ( 1000, 300, 3.67e-3
+    , "data20130219/ADMXQLD311MST300.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 1000, 400, 3.67e-3
+    , "data20130219/ADMXQLD311MST400.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
   , ( 1000, 500, 3.67e-3
-    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ500.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+    , "data20130219/ADMXQLD311MST500.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
   , ( 1000, 600, 3.67e-3
-    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ600.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+    , "data20130219/ADMXQLD311MST600.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
   , ( 1000, 700, 3.67e-3
-    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ700.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+    , "data20130219/ADMXQLD311MST700.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
   , ( 1000, 800, 3.67e-3
-    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ800.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz") ] 
+    , "data20130219/ADMXQLD311MST800.0MG1000.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz") ]
 
-template = 
-  " ({{mgluino}},{{msquark}}) & {{xsec}} & {{totnum}} & {{singlelep3}} & {{singlelep4}} & {{singlelepsoft}} & {{multilep2}} & {{multilep4}} \\\\\n \
-  \ @4.7 fb$^{-1}$ &  &  & {{normsinglelep3}} & {{normsinglelep4}} & {{normsinglelepsoft}} & {{normmultilep2}} & {{normmultilep4}} \\\\\n \
-  \\\hline\n"
 
-normalize :: Double -> Int -> Int -> Maybe Double
-normalize xsec totnum x = 
-  if x /= 0 then Just (xsec*4.7e3/(fromIntegral totnum)*(fromIntegral x)) else Nothing 
+filelist900 = 
+  [ ( 900, 100, 0.0101
+    , "data20130221/ADMXQLD311MST100.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 200, 0.0101
+    , "data20130221/ADMXQLD311MST200.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 300, 0.0101
+    , "data20130219/ADMXQLD311MST300.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 400, 0.0101
+    , "data20130219/ADMXQLD311MST400.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 500, 0.0101
+    , "data20130219/ADMXQLD311MST500.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 600, 0.0101
+    , "data20130219/ADMXQLD311MST600.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 700, 0.0101
+    , "data20130219/ADMXQLD311MST700.0MG900.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
 
-  
+filelist800 = 
+  [ ( 800, 100, 0.0290
+    , "data20130221/ADMXQLD311MST100.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 200, 0.0290
+    , "data20130221/ADMXQLD311MST200.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 300, 0.0290
+    , "data20130221/ADMXQLD311MST300.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 400, 0.0290
+    , "data20130221/ADMXQLD311MST400.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 500, 0.0290
+    , "data20130221/ADMXQLD311MST500.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 600, 0.0290
+    , "data20130221/ADMXQLD311MST600.0MG800.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+
+filelist700 = 
+  [ ( 700, 100, 0.0885
+    , "data20130221/ADMXQLD311MST100.0MG700.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 700, 200, 0.0885
+    , "data20130221/ADMXQLD311MST200.0MG700.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 700, 300, 0.0885
+    , "data20130221/ADMXQLD311MST300.0MG700.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 700, 400, 0.0885
+    , "data20130221/ADMXQLD311MST400.0MG700.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 700, 500, 0.0885
+    , "data20130221/ADMXQLD311MST500.0MG700.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+
+
+filelist600 = 
+  [ ( 600, 100, 0.295
+    , "data20130221/ADMXQLD311MST100.0MG600.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 600, 200, 0.295
+    , "data20130221/ADMXQLD311MST200.0MG600.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 600, 300, 0.295
+    , "data20130221/ADMXQLD311MST300.0MG600.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 600, 400, 0.295
+    , "data20130221/ADMXQLD311MST400.0MG600.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+filelist300400500 = 
+  [ ( 300, 100, 32.0
+    , "data20130219/ADMXQLD311MST100.0MG300.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 400, 100, 5.14
+    , "data20130219/ADMXQLD311MST100.0MG400.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 400, 200, 5.14
+    , "data20130219/ADMXQLD311MST200.0MG400.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 100, 1.12
+    , "data20130219/ADMXQLD311MST100.0MG500.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 200, 1.12
+    , "data20130219/ADMXQLD311MST200.0MG500.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 300, 1.12
+    , "data20130219/ADMXQLD311MST300.0MG500.0MSQ50000.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+
+scharm300400500 = 
+  [ ( 300, 100, 32.0
+    , "data20130222/ADMXQLD211MST50000.0MG300.0MSQ100.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 400, 100, 5.14
+    , "data20130222/ADMXQLD211MST50000.0MG400.0MSQ100.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 400, 200, 5.14
+    , "data20130222/ADMXQLD211MST50000.0MG400.0MSQ200.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 100, 1.12
+    , "data20130222/ADMXQLD211MST50000.0MG500.0MSQ100.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 200, 1.12
+    , "data20130222/ADMXQLD211MST50000.0MG500.0MSQ200.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 500, 300, 1.12
+    , "data20130222/ADMXQLD211MST50000.0MG500.0MSQ300.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+scharm800 = 
+  [ ( 800, 200, 0.0290
+    , "data20130222/ADMXQLD211MST50000.0MG800.0MSQ200.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 300, 0.0290
+    , "data20130222/ADMXQLD211MST50000.0MG800.0MSQ300.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 400, 0.0290
+    , "data20130222/ADMXQLD211MST50000.0MG800.0MSQ400.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 500, 0.0290
+    , "data20130222/ADMXQLD211MST50000.0MG800.0MSQ500.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 800, 600, 0.0290
+    , "data20130222/ADMXQLD211MST50000.0MG800.0MSQ600.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+scharm900 = 
+  [ ( 900, 200, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ200.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 300, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ300.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 400, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ400.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 500, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ500.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 600, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ600.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 900, 700, 0.0101
+    , "data20130222/ADMXQLD211MST50000.0MG900.0MSQ700.0_gluinopair_scharmdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  ]
+
+scharm1000 = 
+  [ ( 1000, 400, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ400.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 1000, 500, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ500.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 1000, 600, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ600.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 1000, 700, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ700.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz")
+  , ( 1000, 800, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ800.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz") ]
+
+
+
+
+
+
 main = do 
   h <- openFile "output.dat" WriteMode 
-  mapM_ (analysis h) filelist
+  hPutStr h header
+  mapM_ (analysis h) scharm1000
+  hPutStr h footer 
   hClose h 
 
-
-analysis :: Handle -> (Double,Double,Double,FilePath) -> IO ()
-analysis h (mgluino,msquark,xsec,fn) = do 
-  putStrLn "atlas counting"
-  -- args <- getArgs
-  -- when (length args /= 1) $ error "./parsertest filename"
-  -- let fn = args !! 0 
-  --     basename = takeBaseName fn 
- 
-  bstr <- LB.readFile fn 
-  let unzipped = decompress bstr 
-
-      evts = parsestr unzipped
-      signalevts = map (preselect HardLepton . taubjetMerge) evts 
-  -- print $ map meff signalevts
-      classified = mapMaybe classifyEvent signalevts 
-  -- print (length evts) 
-  putStrLn fn 
-
-  let totnum = length evts
-      singlelep3 = (length . filter isSingleLep3) classified
-      singlelep4 = (length . filter isSingleLep4) classified
-      singlelepsoft = (length . filter isSingleLepSoft) classified
-      multilep2 = (length . filter isMultiLep2) classified 
-      multilep4 = (length . filter isMultiLep4) classified 
-  putStrLn $ "total number = " ++ show totnum 
-  putStrLn $ "single lep 3 = " ++ show singlelep3 
-  putStrLn $ "single lep 4 = " ++ show singlelep4 
-  putStrLn $ "single lep soft = " ++ show singlelepsoft
-  putStrLn $ "multi lep 2 = " ++ show multilep2
-  putStrLn $ "multi lep 4 = " ++ show multilep4 
-  let normalize_ = normalize xsec totnum 
-  let context "totnum" = MuVariable totnum 
-      context "singlelep3" = MuVariable singlelep3
-      context "singlelep4" = MuVariable singlelep4 
-      context "singlelepsoft" = MuVariable singlelepsoft
-      context "multilep2" = MuVariable multilep2 
-      context "multilep4" = MuVariable multilep4 
-      context "normsinglelep3" = (MuVariable . sciformat . normalize_) singlelep3
-      context "normsinglelep4" = (MuVariable . sciformat . normalize_) singlelep4
-      context "normsinglelepsoft" = (MuVariable . sciformat . normalize_) singlelepsoft
-      context "normmultilep2" = (MuVariable . sciformat . normalize_) multilep2
-      context "normmultilep4" = (MuVariable . sciformat . normalize_) multilep4
-
-
-      context "mgluino" = MuVariable (floor mgluino :: Int)
-      context "msquark" = MuVariable (floor msquark :: Int) 
-      context "xsec" = MuVariable (sciformat (Just xsec))
-
-      -- ("$" ++ ((T.unpack . toLazyText . fixed 2 . getBody10) xsec) ++ "\\times 10^{" ++ (show (getExponent10 xsec)) ++ "}$" :: String)
-      context _ = MuNothing
-  res <- hastacheStr (defaultConfig {muEscapeFunc=emptyEscape}) (encodeStr template) (mkStrContext context) 
-  LB.hPutStr h res
- 
   
-
-    
-  -- LB.putStrLn (LB.take 100 unzipped) 
-
 
---
+++ HEP/Physics/Analysis/ATLAS/SUSY/Format.hs
@@ -0,0 +1,128 @@
+{-# LANGUAGE RecordWildCards, GADTs #-}
+
+module HEP.Physics.Analysis.ATLAS.SUSY.Format where
+
+import           Codec.Compression.GZip
+import           Control.Monad
+import qualified Data.ByteString.Lazy.Char8 as LB
+import           Data.Maybe
+import qualified Data.Text.Lazy as T
+import           Data.Text.Lazy.Builder 
+import           Data.Text.Format 
+-- import           System.Environment
+import           System.FilePath
+import           System.IO 
+import           Text.Hastache
+import           Text.Hastache.Context
+-- 
+import           HEP.Parser.LHCOAnalysis.Parse
+-- 
+import           HEP.Physics.Analysis.ATLAS.SUSY
+-- 
+import Debug.Trace
+
+log10 x = log x / log 10
+
+getExponent10 x = floor (log10 x)
+
+getBody10 x = 10**(log10 x - fromIntegral (getExponent10 x))
+
+sciformat (Just x) = 
+  let e = getExponent10 x
+      b = getBody10 x 
+      -- trunced = (fromIntegral (floor (b*100)) / 100.0) * (10.0**fromIntegral e) 
+  in if e `elem` [-2,-1,0,1,2] 
+     then (T.unpack . toLazyText . fixed (2+(-e))) x
+     else "$" ++ ((T.unpack . toLazyText . fixed 2 . getBody10) x) ++ "\\times 10^{" 
+                      ++ (show e) ++ "}$" 
+sciformat (Nothing) = "0"
+
+{-
+filelist = 
+  [ ( 1000, 400, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ400.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+  , ( 1000, 500, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ500.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+  , ( 1000, 600, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ600.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+  , ( 1000, 700, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ700.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz" )
+  , ( 1000, 800, 3.67e-3
+    , "data20130222/ADMXQLD211MST50000.0MG1000.0MSQ800.0_gluinopair_stopdecayfull_LHC7ATLAS_NoMatch_NoCut_Cone0.4_Set1_pgs_events.lhco.gz") ] 
+-}
+
+header = 
+ " \\begin{tabular}{cccccccc} \n \
+ \ \\hline \n \
+ \ \\hline \n \
+ \ $(m_{\\tilde{g}},m_{\\tilde{c}})$ & ~~~~~~$\\sigma_{\\tilde{g}\\tilde{g}}$~~~~~ & total & \\multicolumn{3}{c}{single lepton} & \\multicolumn{2}{c} {multi lepton}  \\\\ \n \
+ \   &  &  & ~~3 jets~~ & ~~4 jets~~ & ~~~soft~~~ & ~~2 jets~~ & ~~4 jets~~ \\\\ \n \
+ \ \\hline \n \
+ \ \\hline \n" 
+
+footer = "\\end{tabular}\n"
+
+template = 
+  " ({{mgluino}},{{msquark}}) & {{xsec}} & {{totnum}} & {{singlelep3}} & {{singlelep4}} & {{singlelepsoft}} & {{multilep2}} & {{multilep4}} \\\\\n \
+  \ @4.7 fb$^{-1}$ &  &  & {{normsinglelep3}} & {{normsinglelep4}} & {{normsinglelepsoft}} & {{normmultilep2}} & {{normmultilep4}} \\\\\n \
+  \\\hline\n"
+
+normalize :: Double -> Int -> Int -> Maybe Double
+normalize xsec totnum x = 
+  if x /= 0 then Just (xsec*4.7e3/(fromIntegral totnum)*(fromIntegral x)) else Nothing 
+
+
+{-  
+main = do 
+  h <- openFile "output.dat" WriteMode 
+  mapM_ (analysis h) filelist
+  hClose h 
+-}
+
+analysis :: Handle -> (Double,Double,Double,FilePath) -> IO ()
+analysis h (mgluino,msquark,xsec,fn) = do 
+  putStrLn "atlas counting"
+  -- args <- getArgs
+  -- when (length args /= 1) $ error "./parsertest filename"
+  -- let fn = args !! 0 
+  --     basename = takeBaseName fn 
+ 
+  bstr <- LB.readFile fn 
+  let unzipped = decompress bstr 
+      evts = parsestr unzipped
+      signalevts = map (preselect HardLepton . taubjetMerge) evts 
+      classified = mapMaybe classifyEvent signalevts 
+      -- 
+      totnum = length evts
+      singlelep3 = (length . filter isSingleLep3) classified
+      singlelep4 = (length . filter isSingleLep4) classified
+      singlelepsoft = (length . filter isSingleLepSoft) classified
+      multilep2 = (length . filter isMultiLep2) classified 
+      multilep4 = (length . filter isMultiLep4) classified 
+  putStrLn $ "total number = " ++ show totnum 
+  putStrLn $ "single lep 3 = " ++ show singlelep3 
+  putStrLn $ "single lep 4 = " ++ show singlelep4 
+  putStrLn $ "single lep soft = " ++ show singlelepsoft
+  putStrLn $ "multi lep 2 = " ++ show multilep2
+  putStrLn $ "multi lep 4 = " ++ show multilep4 
+  let normalize_ = normalize xsec totnum 
+  let context "totnum" = MuVariable totnum 
+      context "singlelep3" = MuVariable singlelep3
+      context "singlelep4" = MuVariable singlelep4 
+      context "singlelepsoft" = MuVariable singlelepsoft
+      context "multilep2" = MuVariable multilep2 
+      context "multilep4" = MuVariable multilep4 
+      context "normsinglelep3" = (MuVariable . sciformat . normalize_) singlelep3
+      context "normsinglelep4" = (MuVariable . sciformat . normalize_) singlelep4
+      context "normsinglelepsoft" = (MuVariable . sciformat . normalize_) singlelepsoft
+      context "normmultilep2" = (MuVariable . sciformat . normalize_) multilep2
+      context "normmultilep4" = (MuVariable . sciformat . normalize_) multilep4
+      context "mgluino" = MuVariable (floor mgluino :: Int)
+      context "msquark" = MuVariable (floor msquark :: Int) 
+      context "xsec" = MuVariable (sciformat (Just xsec))
+      context _ = MuNothing
+  res <- hastacheStr (defaultConfig {muEscapeFunc=emptyEscape}) (encodeStr template) (mkStrContext context) 
+  LB.hPutStr h res
+
+
+
