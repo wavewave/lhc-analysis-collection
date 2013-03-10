c884657e780bf3a3b828966a8be574b6ed922609
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-10 05:57:58 -0400

    move general indexed monad routines and jet merging routines into Common

--- lib/HEP/Physics/Analysis/ATLAS/Common.hs
+++ lib/HEP/Physics/Analysis/ATLAS/Common.hs
@@ -17,9 +17,58 @@
 module HEP.Physics.Analysis.ATLAS.Common where 
 
 import Control.Applicative ((<$>),(<*>))
+import Control.Monad.Indexed
+import Control.Monad.Indexed.State
+import Control.Monad.Indexed.Trans
 --
 import HEP.Parser.LHCOAnalysis.PhysObj
 
+
+-----------------------------
+-- indexed monad utilities --
+-----------------------------
+
+(>>>) :: (IxMonad m) => m i j a -> m j k b -> m i k b 
+a >>> b = a >>>= \_ -> b
+
+infixr 9 >>> 
+
+-- | guard for indexed monad
+iguard :: IxMonadPlus m => Bool -> m i i () 
+iguard True = ireturn () 
+iguard False = imzero 
+
+
+---------------------------------------
+-- merging taus and b-jets into jets --
+---------------------------------------
+
+data RawEv = Raw PhyEventClassified 
+
+data JetMergedEv = JetMerged PhyEventClassified 
+
+class GetJetMerged a where 
+  getJetMerged :: a -> JetMergedEv
+
+-- 
+
+taubjetMergeIx :: RawEv -> JetMergedEv
+taubjetMergeIx (Raw PhyEventClassified {..}) =  
+  JetMerged (PhyEventClassified { eventid = eventid 
+                     , photonlst = photonlst
+                     , electronlst = electronlst
+                     , muonlst = muonlst
+                     , taulst = [] 
+                     , jetlst = ptordering
+                                ( jetlst 
+                                  ++ map ((,) <$> fst <*> tau2Jet.snd) taulst 
+                                  ++ map ((,) <$> fst <*> bJet2Jet.snd) bjetlst )
+                     , bjetlst = []
+                     , met = met })
+
+
+
+-- 
 
 tau2Jet :: PhyObj Tau -> PhyObj Jet
 tau2Jet (ObjTau x _ _) = ObjJet x 1.777 1
@@ -48,4 +97,6 @@ mt (pt1x,pt1y) (pt2x,pt2y) = sqrt (2.0*pt1*pt2-2.0*pt1x*pt2x-2.0*pt1y*pt2y)
   where pt1 = sqrt (pt1x*pt1x+pt1y*pt1y)
         pt2 = sqrt (pt2x*pt2x+pt2y*pt2y)
         -- cosph = (pt1x*pt2x + pt1y*pt2y)/
+
+
 
