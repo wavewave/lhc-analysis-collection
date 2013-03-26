0c50841f0c36ac3b2eff46a9b643d2d8d4e12e54
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-26 18:09:53 -0400

    pass errors

--- test/checkfile2.hs
+++ test/checkfile2.hs
@@ -48,7 +48,7 @@ main = do
     cr <- (EitherT . liftM (maybeToEither "getCredential")) (getCredential priv pass)
     let wdavcfg = WebDAVConfig cr wdavroot 
         wdavrdir = WebDAVRemoteDir "montecarlo/admproject/smbkg/wp0123" 
-        bnames = map (\x -> "SM_wp0123j_LHC7ATLAS_MLM_DefCut_AntiKT0.4_NoTau_Set" ++ show x) [1]
+        bnames = map (\x -> "SM_wp0123j_LHC7ATLAS_MLM_DefCut_AntiKT0.4_NoTau_Set" ++ show x) [1..100] -- [1..4500]
     mapM (\nm -> getXSecNCount wdavcfg wdavrdir nm >>= getJSONFileAndUpload wdavcfg wdavrdir nm) bnames 
   print r
 
@@ -66,7 +66,8 @@ getJSONFileAndUpload wdavcfg wdavrdir basename mr =
      liftIO $ removeFile fn 
 
 getXSecNCount :: WebDAVConfig -> WebDAVRemoteDir -> String -> EitherT String IO (Maybe CrossSectionAndCount)
-getXSecNCount wdavcfg wdavrdir bname = 
+getXSecNCount wdavcfg wdavrdir bname = do 
+    liftIO $ print bname 
     runMaybeT $ do 
       x <- MaybeT $ xsec wdavcfg wdavrdir bname
       c <- MaybeT $ count wdavcfg wdavrdir bname 
@@ -79,11 +80,18 @@ getXSecNCount wdavcfg wdavrdir bname =
 xsec :: WebDAVConfig -> WebDAVRemoteDir -> String -> EitherT String IO (Maybe Double) 
 xsec wdavcfg wdavrdir bname = do  
     let fp = bname ++ "_pythia.log"
-    boolToMaybeM (liftIO (doesFileExistInDAV wdavcfg wdavrdir fp)) $ do 
-      liftIO ( downloadFile wdavcfg wdavrdir fp ) 
-      val <- EitherT (getXSecFromPythiaLog fp)
-      liftIO ( removeFile fp )
-      return val 
+    b <- (liftIO (doesFileExistInDAV wdavcfg wdavrdir fp))
+    if b 
+      then do 
+        liftIO ( downloadFile wdavcfg wdavrdir fp ) 
+        eval <- liftIO $ getXSecFromPythiaLog fp
+        case eval of
+          Left _ -> return Nothing 
+          Right val -> do       
+            liftIO ( removeFile fp )
+            return (Just val)
+      else return Nothing
+
 
 -- | get number of events
 count :: WebDAVConfig -> WebDAVRemoteDir -> String -> EitherT String IO (Maybe Int)
