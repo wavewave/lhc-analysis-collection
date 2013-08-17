f9f061edc76cd48d199c596d47b310598719bc4a
Ian-Woo Kim <ianwookim@gmail.com>
2013-08-16 19:41:06 -0700

    picture drawing function updated

--- pic/makemesh.hs
+++ pic/makemesh.hs
@@ -8,15 +8,22 @@ parse str = let x:y:z:_ = splitOn "," str
             in (read x, read y, read z) 
 
 
-fillzero xs = let (x,y,_) = last xs 
-              in xs ++ [ (x,y',0) | y' <- [y+50,y+100..1500] ]  
+fillzero inc limit xs = let (x,y,_) = last xs 
+                        in xs ++ [ (x,y',0) | y' <- [y+inc,y+2*inc..limit] ]  
 
 
 main = do 
   args <- getArgs 
   str <- readFile (args !! 0)
+  let inc = read (args !! 1) :: Double
+      limit = read (args !! 2) :: Double 
 
-  let ls = (map fillzero . filter (not.null) . fmap (fmap parse) . splitWhen null . map (dropWhile (== ' ')) . lines) str 
+  let ls = (map (fillzero inc limit) 
+           . filter (not.null) 
+           . fmap (fmap parse) 
+           . splitWhen null 
+           . map (dropWhile (== ' ')) 
+           . lines) str 
   
 
   mapM_ (mapM_ (\(x,y,z) -> putStrLn (show x ++ ", " ++ show y ++ ", " ++ show z)) >=> \_ -> putStrLn " ")  ls
