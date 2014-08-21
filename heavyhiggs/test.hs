3d45060d88490e2b89add76c12a3e693cefe5719
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-21 23:27:00 +0200

    version 20140821

--- heavyhiggs/test.hs
+++ heavyhiggs/test.hs
@@ -10,6 +10,7 @@ import           Control.Monad.Trans.Maybe
 import qualified Data.ByteString.Lazy.Char8 as LB
 import           Data.Default       
 import qualified Data.Foldable as F
+import           Data.List (foldl')
 import           System.Environment
 import           System.IO as IO
 -- 
@@ -78,30 +79,69 @@ eventsets = [ EventSet psetup param (rsetupgen x) | x <- [1..1000] ]
 rdir :: WebDAVRemoteDir
 rdir = WebDAVRemoteDir "newtest2"
 
+checkAndDownload :: WebDAVConfig -> WebDAVRemoteDir -> FilePath -> MaybeT IO ()
+checkAndDownload cfg rdir fpath = do
+    b <- liftIO $ doesFileExistInDAV cfg rdir fpath
+    guard b
+    liftIO $ downloadFile False cfg rdir fpath 
+    liftIO $ putStrLn $ fpath ++ " is successfully downloaded"
+
+{- 
+main :: IO ()
+main = do 
+  print $   combine [("ttest",1,2,3,4,5,[(100,33),(200,34)])
+                    ,("ttest2",1,2,3,4,5,[(100,47),(200,39)]) ]
+-}
+
+
+
 main' :: IO ()
 main' = do
     args <- getArgs 
     let pkey = args !! 0 
         pswd = args !! 1
         whost = args !! 2
-    let fpaths = map (++ "_pgs_events.lhco.gz") . map (\x -> makeRunName psetup param (rsetupgen x)) $ [1..5] -- ..1000] 
+    let fpaths = map (++ "_pgs_events.lhco.gz") . map (\x -> makeRunName psetup param (rsetupgen x)) $ [901..1000] 
     Just cr <- getCredential pkey pswd
     let cfg = WebDAVConfig { webdav_credential = cr, webdav_baseurl = whost }
     withFile "dummy.txt" WriteMode $ \h -> 
       runMaybeT $ do  
-        F.forM_ fpaths $ \fpath -> do
-          b <- liftIO $ doesFileExistInDAV cfg rdir fpath
-          guard b
-          liftIO $ downloadFile False cfg rdir fpath 
-          liftIO $ putStrLn $ fpath ++ " is successfully downloaded"
-          analysis h fpath
+        r <- flip mapM fpaths $ \fpath -> do
+          -- checkAndDownload cfg rdir fpath 
+          {- analysis h fpath -}
+          analysis fpath 
+        liftIO $ print (combine r)
+        return r
     return ()
 
 main :: IO ()
-main = runMaybeT (analysis stdout "fourtopsimpl_pgs_events.lhco.gz") >> return ()
+main = runMaybeT (analysis "fourtopsimpl_pgs_events.lhco.gz") >>= print -- return ()
+
+main'' :: IO ()
+main'' = do
+  str <- readFile "resultfinal.txt"
+  -- print (lines str) 
+  let rf = read :: String -> (Int,Int,Int,Int,Int,[(Double,Int)])
+   
+  print $ lines str !! 1
+  print $  rf (lines str !! 1)
+  
+  let rs = map rf . take 10 $ lines str
+  print (combine2 rs)
 
 
-analysis h fpath = do
+combine :: [(String,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,[(Double,Int)])
+combine  = foldl' f (0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
+  where f (b1,b2,b3,b4,b5,acc) (_,a1,a2,a3,a4,a5,lst) = 
+              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,zipWith (\(x1,y1) (x2,y2) -> (x1,y1+y2)) acc lst)
+
+combine2 :: [(Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,[(Double,Int)])
+combine2  = foldl' f (0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
+  where f (b1,b2,b3,b4,b5,acc) (a1,a2,a3,a4,a5,lst) = 
+              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,zipWith (\(x1,y1) (x2,y2) -> (x1,y1+y2)) acc lst)
+
+
+analysis fpath = do
     liftIO $ putStrLn $ "analyzing " ++ fpath 
     bstr <- liftIO $ LB.readFile fpath
     let unzipped = decompress bstr
@@ -111,25 +151,49 @@ analysis h fpath = do
         -- evt = head mergedevts :: PhyEventNoTau
         --- evt' = mergeBJetFromNoTauEv evt
         fullmergedevts = map mergeBJetFromNoTauEv taumergedevts
+
+        etarangeevts = map filterEtaRange fullmergedevts
+
         test = (,,) <$> checkj <*> checkl <*> checkj2
         pass1 x = view (_1._1) x
         pass2 x = pass1 x && view (_1._2) x
         pass3 x = pass1 x && pass2 x && view (_1._3) x
         pass4 x = pass1 x && pass2 x && pass3 x && view _2 x
-        pass5 x = pass1 x && pass2 x && pass3 x && pass4 x && view (_3._1) x
-        pass6 x = pass1 x && pass2 x && pass3 x && pass4 x && pass5 x && view (_3._2) x
+        -- pass5 x = pass1 x && pass2 x && pass3 x && pass4 x && view (_3._1) x
+        -- pass6 x = pass1 x && pass2 x && pass3 x && pass4 x && pass5 x && view (_3._2) x
 
-        pass1evts = filter (pass1.test) fullmergedevts
-        pass2evts = filter (pass2.test) fullmergedevts
-        pass3evts = filter (pass3.test) fullmergedevts
-        pass4evts = filter (pass4.test) fullmergedevts
-        pass5evts = filter (pass5.test) fullmergedevts
-        pass6evts = filter (pass6.test) fullmergedevts
+        pass1evts = filter (pass1.test) etarangeevts -- fullmergedevts
+        pass2evts = filter (pass2.test) etarangeevts -- fullmergedevts
+        pass3evts = filter (pass3.test) etarangeevts -- fullmergedevts
+        pass4evts = filter (pass4.test) etarangeevts -- fullmergedevts
+        -- pass5evts = filter (pass5.test) etarangeevts -- fullmergedevts
+        -- pass6evts = filter (pass6.test) etarangeevts -- fullmergedevts
 
-    liftIO $ IO.hPutStrLn h 
-      (fpath ++ ": " ++ show (length fullmergedevts, length pass1evts,length pass2evts,length pass3evts,length pass4evts,length pass5evts,length pass6evts)) 
+        assoclist = map (\x -> (x, (length . filter (ptlcut x)) pass4evts)) [10,20..300] 
+    -- liftIO $ print assoclist
+    return (fpath, length fullmergedevts, length pass1evts, length pass2evts, length pass3evts, length pass4evts, assoclist)
+
+    {- liftIO $ IO.hPutStrLn h 
+      (fpath ++ ": " ++ show (length fullmergedevts, length pass1evts, length pass2evts, length pass3evts, length pass4evts)
+                     ++ show assoclist
+      ) -}
+
+       -- show (length fullmergedevts, length pass1evts,length pass2evts,length pass3evts,length pass4evts,length pass5evts,length pass6evts)) 
 
 
+-- countFiltered p lst = length (filter p lst)
+
+ptlcut :: Double -> PhyEventNoTauNoBJet -> Bool
+ptlcut cut ev = let lst = view leptons ev
+                    f x = abs (eta x) < 1.5 && pt x > cut   
+                in  any f lst
+
+-- (not . null . filter (\x -> abs x < 1.5)) etalst
+
+
+
+
+filterEtaRange = over jets (filter (\x -> abs (eta (JO_Jet x)) < 2.5)) 
 
 
 
