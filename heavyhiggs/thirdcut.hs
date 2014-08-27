2e82164cd0f199c7dfa623a0fc3a08a2eab1a80c
Ian-Woo Kim <ianwookim@gmail.com>
2014-08-27 15:10:13 +0200

    code clean a little bit in thirdcut.hs

--- heavyhiggs/thirdcut.hs
+++ heavyhiggs/thirdcut.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-import           Codec.Compression.GZip
 import qualified Codec.Zlib as Zlib
 import           Control.Applicative
 import           Control.DeepSeq
@@ -12,22 +11,13 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Maybe
 import qualified Data.ByteString.Lazy.Char8 as LB
 import           Data.Default       
-import qualified Data.Foldable as F
 import           Data.List (foldl')
 import           Data.Maybe (catMaybes)
 import qualified Data.Traversable as Tr
-import           Pipes
-import qualified Pipes.Attoparsec as PA
 import qualified Pipes.ByteString  as PB
-import           Pipes.Core
-import           Pipes.Parse
-import qualified Pipes.Text.IO as PTIO
-import qualified Pipes.Prelude as PP
 import qualified Pipes.Zlib as PZ
-import           System.Environment
 import           System.IO as IO
 -- 
-import HEP.Automation.EventGeneration.Config
 import HEP.Automation.MadGraph.Model
 import HEP.Automation.MadGraph.Model.SM
 import HEP.Automation.MadGraph.SetupType
@@ -107,59 +97,34 @@ main = do
                  runMaybeT $ do 
                    r' <- analysis bstr
                    r' `deepseq` return r' -- (fpath,r')
-      print (combine3 lst)
-
-
-{-       runMaybeT $ do  
-        r <- flip mapM fpaths $ \fpath -> do
-          r' <- analysis fpath 
-          liftIO $ print r'
-          return r'
-        liftIO $ print (combine r)
-        return r
-    return ()
--}
+      hPutStrLn hout (show (combine3 lst))
 
 {- 
 main1 :: IO ()
 main1 = runMaybeT (analysis "fourtopsimpl_pgs_events.lhco.gz") >>= print 
 
-main2 :: IO ()
-main2 = do
-  str <- readFile "result4.txt"
-
-  let rf = read :: String -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
-   
-  print $ lines str !! 1
-  print $  rf (lines str !! 1)
-  
-  let rs = map rf . take 10 $ lines str
-  print (combine2 rs)
 -}
 
 combine :: [(String,Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
 combine  = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
   where f (b1,b2,b3,b4,b5,b6,acc) (_,a1,a2,a3,a4,a5,a6,lst) = 
-              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (x2,y2) -> (x1,y1+y2)) acc lst)
+              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
 
 combine2 :: [(Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
 combine2  = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
   where f (b1,b2,b3,b4,b5,b6,acc) (a1,a2,a3,a4,a5,a6,lst) = 
-              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (x2,y2) -> (x1,y1+y2)) acc lst)
+              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
 
 combine3 :: [(Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
 combine3 = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
   where f arg1@(!b1,!b2,!b3,!b4,!b5,!b6,!acc) arg2@(!a1,!a2,!a3,!a4,!a5,!a6,!lst) = 
               arg1 `seq` arg2 `seq` 
-                (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (x2,y2) -> (x1,y1+y2)) acc lst)
+                (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
 
 
 analysis :: (MonadIO m) => LB.ByteString -> MaybeT m (Int,Int,Int,Int,Int,Int,[(Double,Int)]) 
 analysis bstr = do
-    -- liftIO $ putStrLn $ "analyzing " ++ fpath 
-    -- bstr <- liftIO $ LB.readFile fpath
-    let -- unzipped = decompress bstr
-        unmergedevts = parsestr bstr -- unzipped
+    let unmergedevts = parsestr bstr
         taumergedevts = map mkPhyEventNoTau unmergedevts
         fullmergedevts = map mergeBJetFromNoTauEv taumergedevts
 
@@ -184,13 +149,15 @@ analysis bstr = do
 
 
 ptlcut :: Double -> PhyEventNoTauNoBJet -> Bool
-ptlcut cut ev = let lst = view leptons ev
-                    f x = abs (eta x) < 1.5 && pt x > cut   
-                in  any f lst
+ptlcut cutv ev = let lst = view leptons ev
+                     f x = abs (eta x) < 1.5 && pt x > cutv
+                 in  any f lst
 
 
+filterEtaRange :: PhyEventNoTauNoBJet -> PhyEventNoTauNoBJet
 filterEtaRange = over jets (filter (\x -> abs (eta (JO_Jet x)) < 2.5)) 
 
+jetpts :: PhyEventNoTauNoBJet -> [Double]
 jetpts = map (pt . JO_Jet) . view jets
 
 -- jetetas = map (eta . JO_Jet) . view jets
@@ -208,25 +175,13 @@ checkj ev = let ptlst = jetpts ev
                   in case xs of
                        _:_:y:ys -> let b2 = y > 100 
                                    in case ys of 
-                                        _:z:zs -> let b3 = z > 50
-                                                  in (b1,b2,b3)
+                                        _:z:_zs -> let b3 = z > 50
+                                                   in (b1,b2,b3)
                                         _ -> (b1,b2,False)
                        _ -> (b1,False,False)
 
+leptonetas :: PhyEventNoTauNoBJet -> [Double]
 leptonetas = map eta . view leptons
-
-
-{- 
-checkj2 :: PhyEventNoTauNoBJet -> (Bool,Bool)
-checkj2 ev = let etalst = jetetas ev
-             in case etalst of 
-                  x1:x2:x3:x4:xs -> let b1 = abs x1 < 1.5 && abs x2 < 1.5 && abs x3 < 1.5 && abs x4 < 1.5
-                                    in case xs of
-                                         y1:y2:ys -> let b2 = abs y1 <2.5 && abs y2 < 2.5
-                                                     in (b1,b2)
-                                         _ -> (b1,False)
-                  _ -> (False,False)
--}
 
 htcut :: PhyEventNoTauNoBJet -> Bool 
 htcut ev = let ptlst = jetpts ev
