a6a04f92fa94fc2f813fc5e2c99fce3db1f854a5
Ian-Woo Kim <ianwookim@gmail.com>
2014-12-15 16:04:27 +0100

    working optimization finding program

--- heavyhiggs/optimize.hs
+++ heavyhiggs/optimize.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
 import qualified Codec.Zlib as Zlib
 import           Control.Applicative
@@ -26,6 +28,7 @@ import qualified Pipes.ByteString  as PB
 import qualified Pipes.Prelude as PPrelude
 import qualified Pipes.Zlib as PZ
 import           System.IO as IO
+import           Text.Printf
 -- 
 import HEP.Automation.MadGraph.Model
 import HEP.Automation.MadGraph.Model.SM
@@ -96,20 +99,43 @@ checkAndDownload cfg rdir fpath = do
     liftIO $ putStrLn $ fpath ++ " is successfully downloaded"
 
 
-main :: IO ()
-main = do
-    -- let fpaths = map (++ "_pgs_events.lhco.gz") . map (\x -> makeRunName psetup param (rsetupgen x)) $ [1..1000] 
-    let fpaths = ["fourtopsimpl_400_pgs_events.lhco.gz"]
+fourtopsimpl400 = map (\x->"data/fourtopsimpl_400_set" ++ x ++  "_pgs_events.lhco.gz") 
+                      [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ]
+
+fourtopsimpl750 = map (\x->"data/fourtopsimpl_750_set" ++ x ++  "_pgs_events.lhco.gz") 
+                      [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ]
+
+
+data CutChoice = CutChoice { choice_ht :: Double
+                           , choice_ptj1 :: Double
+                           , choice_ptj234 :: Double
+                           , choice_ptj56 :: Double }
+               deriving (Show,Eq,Ord)
+
+testset1 = CutChoice { choice_ht = 1500
+                     , choice_ptj1 = 200
+                     , choice_ptj234 = 100
+                     , choice_ptj56 = 50 } 
+
+testsets = [ CutChoice ht j1 j234 j56 | ht <- [500,800,1000,1200,1500] 
+                                      , j1 <- [100,200,300]
+                                      , j234 <- [50,100..j1]
+                                      , j56 <- [25,50..j234] ]
+
+work :: [FilePath] -> CutChoice -> IO (Int, Int, Int, Int, Int, Int, Int, Int, Int)
+work fpaths CutChoice {..} = do
     hins <- mapM (\fpath -> openFile fpath ReadMode) fpaths  
-    r <- flip runStateT (0,0,0,0,0,0,0,0,0) $ runEffect $ do
+    (_,r) <- flip runStateT (0,0,0,0,0,0,0,0,0) $ runEffect $ do
            F.forM_ fpaths $ \fpath -> do 
              hin <- liftIO $ openFile fpath ReadMode
              pipesLHCOEvent (gunzip hin >-> PPrelude.map T.decodeUtf8)
              liftIO $ hClose hin 
            >-> PPrelude.tee (count _1 >-> PPrelude.drain)
-           >-> PPrelude.tee (countmark 1000 0 >-> PPrelude.drain)
+           >-> PPrelude.tee (countmark 5000 0 >-> PPrelude.drain)
            >-> PPrelude.map (((,) <$> filterEtaRange . mergeBJetFromNoTauEv <*> id) . mkPhyEventNoTau)
-           >-> PPrelude.map (over _2 numOfB . over _1 (\x->(checkj x, checkl x, htcut 1500 x)))
+           >-> PPrelude.map ( over _2 (numOfB choice_ptj56) 
+                            . over _1 
+                               (\x->(checkj (choice_ptj1,choice_ptj234,choice_ptj56) x, checkl x, htcut choice_ht x)))
            >-> PPrelude.filter (view (_1._1._1)) >-> PPrelude.tee (count _2) -- pass1 
            >-> PPrelude.filter (view (_1._1._2)) >-> PPrelude.tee (count _3) -- pass2
            >-> PPrelude.filter (view (_1._1._3)) >-> PPrelude.tee (count _4) -- pass3
@@ -120,10 +146,10 @@ main = do
            >-> PPrelude.filter ((>=3) . view (_2)) >-> PPrelude.tee (count _9) -- 3 bjets, pT_bj > 50 
 
            -- >-> PPrelude.map (view (_1))
-           >-> PPrelude.tee (PPrelude.print >-> PPrelude.drain)
+           -- >-> PPrelude.tee (PPrelude.print >-> PPrelude.drain)
            >-> PPrelude.drain
        
-    print r                              
+    return r
 
 count :: Simple Lens s Int -> Consumer a (StateT s IO) ()
 count l = forever $ do await 
@@ -152,24 +178,25 @@ checkl :: PhyEventNoTauNoBJet -> Bool
 checkl ev = let etalst = leptonetas ev
             in (not . null . filter (\x -> abs x < 1.5)) etalst
 
-checkj :: PhyEventNoTauNoBJet -> (Bool,Bool,Bool)
-checkj ev = let ptlst = jetpts ev
-            in case ptlst of
-                [] -> (False,False,False)
-                x:xs -> 
-                  let b1 = x > 200
-                  in case xs of
-                       _:_:y:ys -> let b2 = y > 100 
-                                   in case ys of 
-                                        _:z:_zs -> let b3 = z > 50
-                                                   in (b1,b2,b3)
-                                        _ -> (b1,b2,False)
-                       _ -> (b1,False,False)
+checkj :: (Double,Double,Double) -> PhyEventNoTauNoBJet -> (Bool,Bool,Bool)
+checkj (j1,j234,j56) ev = 
+    let ptlst = jetpts ev
+    in case ptlst of
+        [] -> (False,False,False)
+        x:xs -> 
+          let b1 = x > j1 -- 200
+          in case xs of
+               _:_:y:ys -> let b2 = y > j234 -- 100 
+                           in case ys of 
+                                _:z:_zs -> let b3 = z > j56 -- 50
+                                           in (b1,b2,b3)
+                                _ -> (b1,b2,False)
+               _ -> (b1,False,False)
 
 
-numOfB :: PhyEventNoTau -> Int
-numOfB ev = let ptlst = (filter (> 50) . map pt . view bjets) ev
-            in length ptlst 
+numOfB :: Double -> PhyEventNoTau -> Int
+numOfB j56 ev = let ptlst = (filter (> j56{- 50 -}) . map pt . view bjets) ev
+                in length ptlst 
 
 leptonetas :: PhyEventNoTauNoBJet -> [Double]
 leptonetas = map eta . view leptons
@@ -190,96 +217,20 @@ countmark marker n = go n
 
 
 
+format :: CutChoice -> (Int, Int, Int, Int, Int, Int, Int, Int, Int) -> String
+format CutChoice {..} (full,c1,c2,c3,le,ht,b1,b2,b3) =
+    printf "%6.1f %6.1f %6.1f %6.1f %7d %7d %7d %7d %7d %7d %7d %7d %7d" choice_ht choice_ptj1 choice_ptj234 choice_ptj56 full c1 c2 c3 le ht b1 b2 b3
 
 
-{-
-  
-main' :: IO ()
-main' = do
-    let fpaths = map (++ "_pgs_events.lhco.gz") . map (\x -> makeRunName psetup param (rsetupgen x)) $ [1..1000] 
-    withFile "dummy.txt" WriteMode $ \hout -> do
-      lst <- ({- fmap catMaybes . -} Tr.forM fpaths) $ \fpath ->  
-               withFile fpath ReadMode $ \hin -> do 
-                 putStrLn fpath
-                 -- let testfold = Foldl.FoldM (\acc _ -> return (acc+1)) (return 0) return
-                 r <- PPrelude.fold (\acc _ -> acc+1) 0 id  ( (pipesLHCOEvent (gunzip hin >-> PPrelude.map T.decodeUtf8) >> return () )
-                                                              >-> PPrelude.tee (countmark 1000 0 >-> PPrelude.drain )
-                                                              >-> PPrelude.map (filterEtaRange . mergeBJetFromNoTauEv . mkPhyEventNoTau)
-                                                              >-> PPrelude.map ((,,) <$> checkj <*> checkl <*> htcut) 
-                                                              >-> PPrelude.tee (PPrelude.print >-> PPrelude.drain)
-                                                              >-> PPrelude.drain
-                                                            )
-                 --                >-> testsum 
-                 --                >-> PPrelude.drain)    --  >-> PB.count
-                 return r 
-                 -- print r
-                 -- print txt
-                 {- runMaybeT $ do 
-                   r' <- analysis bstr
-                   r' `deepseq` return r' -- (fpath,r') -}
-      -- hPutStrLn hout (show (combine3 lst))
-      print (sum lst)
-      return ()
-
--}
+-- main = print testsets
+main :: IO ()
+main = do
+    let (m,n) = (1,340)
+    withFile "output.dat" WriteMode $ \h -> do
+      F.forM_ ((take (n-m+1) . drop (m-1)) testsets) $ \x -> do
+        r <- (x,) <$> work fourtopsimpl400 x
+        let str = uncurry format r
+        putStrLn str
+        hPutStrLn h str
 
 
-
-
-{- 
-main1 :: IO ()
-main1 = runMaybeT (analysis "fourtopsimpl_pgs_events.lhco.gz") >>= print 
-
--}
-
-{-
-combine :: [(String,Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
-combine  = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
-  where f (b1,b2,b3,b4,b5,b6,acc) (_,a1,a2,a3,a4,a5,a6,lst) = 
-              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
-
-combine2 :: [(Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
-combine2  = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
-  where f (b1,b2,b3,b4,b5,b6,acc) (a1,a2,a3,a4,a5,a6,lst) = 
-              (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
-
-combine3 :: [(Int,Int,Int,Int,Int,Int,[(Double,Int)])]  -> (Int,Int,Int,Int,Int,Int,[(Double,Int)])
-combine3 = foldl' f (0,0,0,0,0,0,map (\x->(x,0)) [10,20..300]) 
-  where f arg1@(!b1,!b2,!b3,!b4,!b5,!b6,!acc) arg2@(!a1,!a2,!a3,!a4,!a5,!a6,!lst) = 
-              arg1 `seq` arg2 `seq` 
-                (b1+a1,b2+a2,b3+a3,b4+a4,b5+a5,b6+a6,zipWith (\(x1,y1) (_x2,y2) -> (x1,y1+y2)) acc lst)
-
-
-analysis :: (MonadIO m) => LB.ByteString -> MaybeT m (Int,Int,Int,Int,Int,Int,[(Double,Int)]) 
-analysis lbstr = do
-    
-    let bstr = LB.toStrict lbstr
-        txt = T.decodeUtf8 bstr
-        unmergedevts = case PA.parseOnly lhco txt of -- -- parsestr bstr
-                         Left err -> error "lhco parse error"
-                         Right evts -> evts
-
-        taumergedevts = map mkPhyEventNoTau unmergedevts
-        fullmergedevts = map mergeBJetFromNoTauEv taumergedevts
-
-        etarangeevts = map filterEtaRange fullmergedevts
-
-        test = (,,) <$> checkj <*> checkl <*> htcut
-        pass1 x = view (_1._1) x
-        pass2 x = pass1 x && view (_1._2) x
-        pass3 x = pass1 x && pass2 x && view (_1._3) x
-        pass4 x = pass1 x && pass2 x && pass3 x && view _2 x
-        pass5 x = pass1 x && pass2 x && pass3 x && pass4 x && view _3 x
-
-        pass1evts = filter (pass1.test) etarangeevts 
-        pass2evts = filter (pass2.test) etarangeevts 
-        pass3evts = filter (pass3.test) etarangeevts 
-        pass4evts = filter (pass4.test) etarangeevts 
-        pass5evts = filter (pass5.test) etarangeevts 
-
-        assoclist = map (\x -> (x, (length . filter (ptlcut x)) pass5evts)) [10,20..300] 
-    return (length fullmergedevts, length pass1evts, length pass2evts, length pass3evts, length pass4evts, length pass5evts, assoclist)
-
-
--- pipesAnalysis :: Pipes PhyEventClassified ( ) m r 
--}
